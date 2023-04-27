9b330b314ae63187abd4e9873d7355a0fd062302
Anton Zhuravlev <abramoff.job@gmail.com>
2023-04-27 12:18:28 +0300

    database connect

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 import express from "express"
+const mysql = require("mysql")
 const POST = 5000;
 
+const connection = mysql.createConnection({
+    host: "root",
+    user: "daskis",
+    database: "node_basics",
+    password: "Daskis009"
+});
 const app = express()
 app.use(express.json())
 app.get("/", (req, res) => {
     return res.status(200).json("Работает")
 })
 
-app.listen(POST, () => console.log("success"))
+app.listen(POST, () => {
+    connection.connect((err) => {
+        if (err) {
+            console.log(err)
+            return err;
+        }
+        else {
+            console.log("success")
+        }
+    })
+})
