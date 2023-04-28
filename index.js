f63a3da955be5903a13ecf4966461742a5031955
Anton Zhuravlev <abramoff.job@gmail.com>
2023-04-28 21:09:47 +0300

    Закончил

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express"
 import mysql from "mysql"
-const POST = 5000;
-
+import bodyParser from "body-parser";
+import multer from "multer";
+import cors from "cors"
+const PORT = 5000;
+const upload = multer()
 const connection = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
@@ -9,19 +12,82 @@ const connection = mysql.createConnection({
     password: "Daskis009"
 });
 const app = express()
-app.use(express.json())
-app.get("/", (req, res) => {
-    return res.status(200).json("Работает")
+app.use(bodyParser.json())
+app.use(upload.array()); 
+app.use(express.static('public'));
+app.use(cors())
+app.post("/", (req, res) => {
+    const data = {
+        name: false,
+        email: false,
+        birthYear: false,
+        sex: false,
+        limbs: false,
+        biography: false,
+        superpower: false
+    }
+    const NAME_REGEXT = /^[А-ЯЁ][а-яё]+$/
+    const superpower = {
+        levitation: false,
+        immortality: false,
+        passing: false
+    }
+    const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+    if (req.body) {
+        if (!NAME_REGEXT.test(req.body.name)) {
+            data.name = true;
+        } 
+        if (!EMAIL_REGEXP.test(req.body.email)) {
+            data.email = true;
+        }
+        if (!req.body.birthYear) {
+            data.birthYear = true;
+        }
+        if (!req.body.sex) {
+            data.sex = true;
+        }
+        if (!req.body.limbs) {
+            data.limbs = true;
+        }
+        if (req.body.superpower) {
+            for (let item of req.body.superpower) {
+                superpower[item.value] = true
+            }
+        } 
+        if (req.body.superpower.length == 0) {
+            data.superpower = true
+        }
+        if (!req.body.biography) {
+            data.biography = true;
+        }
+        let f = false;
+        for (const [key, value] of Object.entries(data)) {
+            if (value) {
+                f = true
+            }
+        }
+        if (f) {
+            return res.json({fail: data})
+        } else {
+            try {
+                const answerData = Object.assign(superpower, req.body)
+                delete answerData.superpower
+                connection.query(`INSERT INTO peoples (name, email, birthYear, sex, limbs, levitation, immortality, passing, biography)
+                    VALUES ("${req.body.name.toString()}", "${req.body.email.toString()}", "${req.body.birthYear.toString()}", "${req.body.sex.toString()}","${req.body.limbs.toString()}",${Boolean(superpower.levitation)},${Boolean(superpower.immortality)},${Boolean(superpower.passing)},"${req.body.biography.toString()}");`)
+            return res.json({success: answerData})
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        
+        
+    }
 })
 
-app.listen(POST, () => {
+app.listen(PORT, () => {
     connection.connect((err) => {
         if (err) {
             console.log(err)
-            return err;
-        }
-        else {
-            console.log("success")
         }
     })
 })
